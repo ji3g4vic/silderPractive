7a29f0bb16a3a4a1f3d7c3adf5392f6e7e743643
Vic <ji3g4vic03@gmail.com>
2023-01-10 12:59:04 +0800

    Initial Commit

---
+++ silderPractive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  silderPractive
+//
+//  Created by 王衍智 on 2023/1/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
